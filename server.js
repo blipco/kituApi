23c1d87a86affbaa3177bb21381efe681b1b5992
Bobby <blipco619@gmail.com>
2019-05-02 11:25:17 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+app.use(bodyParser.json());
+
+const app = express();
+const port = (process.env.PORT||3000);
+const url = 'https://www.randomuser.me/api/?format=json&results=1&nat=us';
+let storage = [];
+
+app.get('/users',(req, res) => {
+  axios.get(`${url}`)
+  .then((axiosRes) => {
+    storage = [...axiosRes.data.results];
+    res.status(200).json(storage);
+  })
+  .catch(err => res.status(400).send(err));
+});
+
+app.get('/users/firstname/:firstname',(req, res) => {
+  let firstName = req.params.firstname;
+  for(let i=0;i<storage.length;i++) {
+    if(storage[i].name.first==firstName) {
+      res.status(200).send(storage[i]);
+    }else {
+      res.status(404).json({'message': 'User not found!'});
+    };
+  };
+});
+
+app.post('/users',(req, res) => {
+  storage.push(req.body);
+  res.status(201).send({'message':'User successfully created!'})
+});
+
+app.listen(port,() => console.log(`kituApi is listening on port ${port}`));
